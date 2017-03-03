1101328baa61fe7129d5eeb1fc6f4ee0e1b2fd32
Sebastian Hallum Clarke <s@zibity.com>
2017-03-02 21:12:47 -0500

    Standardising syntax

--- semester.js
+++ semester.js
@@ -1,19 +1,19 @@
 // This script is a model of the Semester object
 
 // Load external dependencies
-var mongoose = require("mongoose");
+var mongoose = require('mongoose')
 
 // Define the semesterSchema
 var semesterSchema = new mongoose.Schema({
-    _id: Number,
-    code: Number,
-    name: String,
-    start_date: String,
-    end_date: String,
-});
+  _id: Number,
+  code: Number,
+  name: String,
+  start_date: String,
+  end_date: String
+})
 
 // Create the Semester model from the courseSchema
-var Semester = mongoose.model('Semester', semesterSchema);
+var Semester = mongoose.model('Semester', semesterSchema)
 
 // Export the Course model
-module.exports = Semester;
+module.exports = Semester
