462798402b46e6bdac4c8ae42a50df17dd13a1be
melyshu <mel.y.shu@gmail.com>
2017-03-11 17:18:23 -0500

    Update from alt files
    All alt- files are now just merged into their originals.

--- public/scripts/loggedin.js
+++ public/scripts/loggedin.js
@@ -1,73 +1,73 @@
-// An example script that shows how the page can make a request to our server
-var $
+// when document loads
+$(document).ready(function () {
 
-// A function to fetch the course data
-var getCourseData = function () {
-  // Prepare the data for the request to the server
-  var query = {
-    $text: {
-      $search: $('#searchbox').val()
-    },
-    semester: $('#semester').val()
+  // function for updating search results
+  var getCourseData = function () {
+    var query = {
+      $text: {
+        $search: $('#searchbox').val()
+      },
+      semester: $('#semester').val()
+    }
+
+    $.post('/api/courses',
+    {
+      query: JSON.stringify(query),
+      sort: $('#sort').val()
+    }, function (courses, status) {
+      // Basic error handling
+      if (status !== 'success') {
+        window.alert('An error occured and your search could not be completed.')
+      }
+
+      // clear results
+      $('#results').html('')
+
+      for (var courseIndex in courses) {
+        var thisCourse = courses[courseIndex]
+
+        $('#results').append('<a class="list-group-item search-result"><div>' +
+                             thisCourse.department + ' ' + thisCourse.catalogNumber +
+                             (thisCourse.distribution == undefined ? '' : ' <span class="label label-info">' + thisCourse.distribution + '</span>') +
+                             '</div><div>' + thisCourse.title + '</div></a>')
+
+        // attach object to DOM element
+        $('#results').children().last()[0].course = thisCourse
+      }
+    })
   }
 
-  // Send the request to the server
-  $.post('/api/courses', {
-    query: JSON.stringify(query),
-    sort: $('#sort').val()
-  }, function (courses, status) {
-    // Basic error handling
-    if (status !== 'success') {
-      window.alert('An error occured and your search could not be completed.')
-    }
-
-    // Empty the results area
-    $('#results').html('')
-
-    // Insert the course results into the area
-    for (var courseIndex in courses) {
-      var thisCourse = courses[courseIndex]
-      $('#results').append('<div><span style="font-weight:bold">' + thisCourse.department + ' ' + thisCourse.catalogNumber + '</span> ' + thisCourse.title + '</div>')
-    }
-  })
-}
-
-// These functions are run once the DOM has loaded
-$(document).ready(function () {
   // Every time a key is pressed inside the #searchbox, call the getCourseData function
   $('#searchbox').keyup(getCourseData)
-  $('#semester, #sort').change(getCourseData)
+  $('#semester').change(getCourseData)
 
+  // displays information in right pane on click of search result
+  $('#results').on('click', 'a.search-result', function() {
+
+    $(".search-result").removeClass("active");
+    $(this).addClass("active")
+
+    $('#disp-title').html('')
+    $('#disp-profs').html('')
+    $('#disp-desc').html('')
+    $('#disp-body').html('')
+
+    var thisCourse = this.course
+    $('#disp-title').append(thisCourse.department + ' ' + thisCourse.catalogNumber +
+                         ' <small>' + thisCourse.title + '</small>')
+
+    $('#disp-desc').append(thisCourse.description)
+
+    $('#disp-body').append('<p>' + thisCourse.description + '</p><p>' + thisCourse.description + '</p><p>' +
+                           thisCourse.description + '</p><p>' + thisCourse.description + '</p><p>' +
+                           thisCourse.description + '</p><p>' +thisCourse.description + '</p>')
+  })
+
+  // load the semesters for the dropdown
   $.get('/api/semesters', function (semesters) {
     for (var semesterIndex in semesters) {
       var thisSemester = semesters[semesterIndex]
       $('#semester').append('<option value="' + thisSemester.code + '">' + thisSemester.name + '</select>')
     }
-  })
-
-  $.ajax({
-    url: '/api/user/favorite',
-    type: 'PUT',
-    data: {
-      course: '1152012875'
-    },
-    success: function (a, b) {
-      console.log(a, b)
-    }
-  })
-
-  $.ajax({
-    url: '/api/user/favorite',
-    type: 'DELETE',
-    data: {
-      course: '1152012874'
-    },
-    success: function (a, b) {
-      console.log(a, b)
-    }
-  })
-
-  $.get('/api/user/favorites', function (a, b) {
-    console.log(a, b)
   })
 })
