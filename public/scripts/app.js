73d923c2e48b126398fb6c9e71ab916a64842fd5
melyshu <mel.y.shu@gmail.com>
2017-04-02 15:19:29 -0400

    import new design

---
+++ public/scripts/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $('#search-pane').resizable({
+    handles: 'e'
+  })
+
+  $('#info-pane').resizable({
+    handles: 'w'
+  })
+
+
+  $('#favorite-courses').css('max-height', '30%')
+
+    // toggle display of favorite things
+  var toggleFavDisplay = function() {
+    var isVisible = $('#favorite-courses').css('display') !== 'none'
+
+    var icon = $('#fav-display-toggle')
+    icon.removeClass(isVisible ? 'fa-minus' : 'fa-plus')
+    icon.addClass(isVisible ? 'fa-plus' : 'fa-minus')
+    $('#favorite-courses').slideToggle()
+  }
+  $('#fav-display-toggle').click(toggleFavDisplay)
+
+  // toggle display of search result things
+  var toggleSearchDisplay = function() {
+    var isVisible = $('#search-results').css('display') !== 'none'
+
+    var icon = $('#search-display-toggle')
+    icon.removeClass(isVisible ? 'fa-minus' : 'fa-plus')
+    icon.addClass(isVisible ? 'fa-plus' : 'fa-minus')
+    $('#favorite-courses').animate({'max-height': (isVisible ? '100%' : '30%')})
+
+    $('#search-results').slideToggle()
+  }
+  $('#search-display-toggle').click(toggleSearchDisplay)
+})
