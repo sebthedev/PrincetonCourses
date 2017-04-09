90f9ac7e9b0a99c2ad66138843227b8e040c577b
Bensu <bnsue1@gmail.com>
2017-04-08 22:49:10 -0400

    resize event listener

--- public/scripts/device.js
+++ public/scripts/device.js
@@ -1,18 +1,37 @@
 
 // Return the height of the window
-var getWindowHeight = function() {
+var windowHeight = function() {
 	var windowHeight = $(window).height();
 	return windowHeight;
 }
 
 // Return the width of the window
-var getWindowWidth = function() {
+var windowWidth = function() {
 	var windowWidth = $(window).width();
 	return windowWidth;
 }
 
 // Return the id of searchbox depending on screen size
 var getDeviceSearchBox = function() {
-	if (getWindowWidth() > 768) { return "#searchbox"; }
+	if (windowWidth() > 768) { return "#searchbox"; }
 	else { return "#mob-searchbox"; }
 }
+
+var onresize = function() {
+	if (windowWidth > 768)
+	{
+	  // //var backButton = '<div><button type="button" onclick="goBackToSearchResults();" class="btn btn-secondary">Back</div></span>'
+	  // $('#search-pane').css("display", "");
+	  // $('#display-pane').css("display", "");
+	  // $('body').css("background-color", "#ffffff");
+	  // $('#backButton').css("display", "none");
+	}
+	if (windowWidth <= 768)
+	{
+	  // //var backButton = '<div><button type="button" onclick="goBackToSearchResults();" class="btn btn-secondary">Back</div></span>'
+	  // $('#search-pane').css("display", "");
+	  // $('#display-pane').css("display", "none");
+	  // $('body').css("background-color", "#dddddd");
+	}
+}
+window.addEventListener("resize", onresize);
