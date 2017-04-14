0c66a23145c4f560cc5a65d161bf3e915b455197
melyshu <mel.y.shu@gmail.com>
2017-04-14 12:59:45 -0400

    Revert "yucky fiddle"
    This reverts commit c51c089344fd0ca22223ff589b299356219d8ea9.

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -53,13 +53,7 @@ var searchForCourses = function () {
         $('#results').append(newDOMResult(result, {"tags": 1}))
       }
     }
-
-    // update active highlighting
-    displayActive()
-
-    // switch to correct screen if in mobile
-    if (document.isMobile) $('#main-pane').slick('slickGoTo', 1)
-  })
+  }).then(displayActive)
 }
 
 // length of non-instructor results (for temporary hiding of instructors)
