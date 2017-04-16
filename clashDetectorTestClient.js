c44bcaac19041cd6c6db6d5caac23b76cc18c6be
Sebastian Hallum Clarke <s@zibity.com>
2017-04-16 16:22:41 -0400

    Changing the test client to use a real user's favorites

--- clashDetectorTestClient.js
+++ clashDetectorTestClient.js
@@ -7,33 +7,39 @@ require('dotenv').config()
 require('./controllers/database.js')
 require('./models/semester.js')
 var courseModel = require('./models/course.js')
+var userModel = require('./models/user.js')
 var clashDetector = require('./courseClashDetector.js')
 
 // Greet the user
 console.log('Testing the Course Clash Detector')
 
 // Define an database query that will yield an array of courses to be used as the "Favorite Courses" list
-const favoriteCoursesQuery = {
-  catalogNumber: '333',
-  semester: 1174
-}
 
 // Define an database query that will yield an array of courses that will be compared against the favorite courses for clashes
 const comparisonCoursesQuery = {
   catalogNumber: '201',
-  semester: 1174
+  semester: 1182
 }
 
 // Execute the database queries
-let favoriteCoursesPromise = courseModel.find(favoriteCoursesQuery).exec()
+let userPromise = userModel.findById('smclarke').populate('favoriteCourses').exec()
 let comparisonCoursesPromise = courseModel.find(comparisonCoursesQuery).exec()
 
 // Wait for both of the database queries to return
-Promise.all([favoriteCoursesPromise, comparisonCoursesPromise]).then(function (results) {
-  let favoriteCourses = results[0]
+Promise.all([userPromise, comparisonCoursesPromise]).then(function (results) {
+  let user = results[0].toObject()
   let courses = results[1]
 
-  console.log('The user\'s favorite courses are:')
+  // Check the user has a favoriteCourses array
+  if (!user.hasOwnProperty('favoriteCourses')) {
+    console.log('User has no favoriteCourses array')
+    process.exit(0)
+  }
+
+  // Extract the user's favorite courses
+  let favoriteCourses = user.favoriteCourses
+
+  console.log('%s\'s favorite courses are:', user._id)
   for (let courseIndex in favoriteCourses) {
     let thisCourse = favoriteCourses[courseIndex]
     console.log('\t' + thisCourse.commonName, thisCourse.semester.name)
@@ -46,7 +52,7 @@ Promise.all([favoriteCoursesPromise, comparisonCoursesPromise]).then(function (r
   }
 
   // Enquire about the clash status of the courses
-  var clashDetectorResponse = clashDetector.detectCourseClash(favoriteCourses, courses, true)
+  var clashDetectorResponse = clashDetector.detectCourseClash(favoriteCourses, courses)
 
   console.log('Clash results:')
   for (let courseIndex in clashDetectorResponse) {
