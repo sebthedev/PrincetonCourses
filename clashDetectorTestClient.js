ffb657369642f5f4de0807fd8d47ababd121701b
cgolner <Cat@nat-oitwireless-inside-vapornet100-b-19474.Princeton.EDU>
2017-04-16 15:07:15 -0400

    Test Client updated somewhat

--- clashDetectorTestClient.js
+++ clashDetectorTestClient.js
@@ -16,13 +16,16 @@ courseModel.find({catalogNumber: '201', semester: 1174}, function (err, courses)
       if (err) {
         console.log(err)
       } else {
-        for (courseIndex in courses) {
-          console.log(courses[courseIndex].department)
-        }
+        console.log("Favorite Courses [Department] 333")
         for (courseIndex in favoriteCourses) {
           console.log(favoriteCourses[courseIndex].department)
         }
         var clashDetectorResponse = clashDetector.detectCourseClash(favoriteCourses, courses, true)
+        console.log("Course Search Clash Results")
+        for (var i = 0; i < clashDetectorResponse.length; i++) {
+          console.log(clashDetectorResponse[i].title)
+          console.log(clashDetectorResponse[i].clash)
+        }
         process.exit(0)
       }
     })
