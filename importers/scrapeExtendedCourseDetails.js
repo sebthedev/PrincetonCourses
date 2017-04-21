9cf60d16c3974aa689b918f744b9ed3329cd780d
Sebastian Hallum Clarke <s@zibity.com>
2017-04-21 13:19:50 -0400

    Fixes socket hangup error

--- importers/scrapeExtendedCourseDetails.js
+++ importers/scrapeExtendedCourseDetails.js
@@ -23,24 +23,33 @@ var loadPage = function (term, courseID, externalCallback) {
   }
 
   // Make the request
-  var req = http.request(options, function (res) {
-    var str = ''
+  try {
+    var req = http.request(options, function (res) {
+      var str = ''
 
-    // Append received data to already received data
-    res.on('data', function (chunk) {
-      str += chunk
+      // Append received data to already received data
+      res.on('data', function (chunk) {
+        str += chunk
+      })
+
+      // Handle data once it has all been received
+      res.on('end', function () {
+        externalCallback(str)
+      })
+
+      res.on('error', function (err) {
+        console.log(err)
+      })
     })
 
-    // Handle data once it has all been received
-    res.on('end', function () {
-      externalCallback(str)
-    })
-
-    res.on('error', function (err) {
+    req.on('error', function (err) {
       console.log(err)
     })
-  })
-  req.end()
+
+    req.end()
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 var extractSingle = function ($, container, title) {
