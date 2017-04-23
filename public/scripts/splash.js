5308c2c97969d6a56f4160a4d417066e17f2e00e
Bensu <bnsue1@gmail.com>
2017-04-22 22:56:23 -0400

    many improvements

--- public/scripts/splash.js
+++ public/scripts/splash.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function(){
-  $('#slideshow').slick({
-  	dots: true,
-  	arrows: true,
-	  infinite: false,
-	  speed: 300,
-	  autoplay: true
-	});
+ //  $('#slideshow').slick({
+ //  	dots: true,
+ //  	arrows: true,
+	//   infinite: false,
+	//   speed: 300,
+	//   autoplay: true
+	// });
 });
