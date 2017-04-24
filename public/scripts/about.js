c47b885f37475820ba73d70552a142d2d68cacab
Bensu <bnsue1@gmail.com>
2017-04-24 15:51:13 -0400

    Tweaking the about page

--- public/scripts/about.js
+++ public/scripts/about.js
@@ -1,4 +1,6 @@
 // initialization
 $(document).ready(function() {
   // nothing for about page at this time
+  	$('.team-member>h4').matchHeight();
+    //$('.team-member-description').matchHeight();
 })
