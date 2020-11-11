33b3f4b44293bac5772232e936bb1069afe417a4
chuckan13 <an.charlie@gmail.com>
2020-11-11 02:18:02 -0500

    debug

--- public/scripts/icon.js
+++ public/scripts/icon.js
@@ -54,7 +54,7 @@ function newHTMLscoreBadge(course, props) {
   else if (isNew) text = 'New'
 
   console.log(course.title)
-  console.log(course.semester)
+  console.log(isTitle)
   console.log(hasScore)
   console.log(isPast)
   console.log(tooltip)
