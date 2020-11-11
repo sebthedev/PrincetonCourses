ed0c24b36e2fc10863c25685ba689ef7527a4d59
Emre Cakir <cakirmehmete@gmail.com>
2020-11-11 00:04:56 -0500

    Update scrapeEvaluations.js

--- importers/scrapeEvaluations.js
+++ importers/scrapeEvaluations.js
@@ -99,7 +99,7 @@ promptly.prompt('Paste the session cookie output from the developer console and 
   require('../controllers/database.js')
   regex_input = "/^(" + query + ")/";
   // Find an array of courses and populate the courses with the course evaluation information from the Registrar. Save the data to the database
-  return courseModel.find({ department: "COS" });
+  return courseModel.find({ department: regex_input });
 }).then(returnedCourses => {
   courses = returnedCourses;
   return promptly.confirm(`You are about to request the course evaluation data for ${courses.length} courses. Are you sure you want to do this? (y/n):`)
